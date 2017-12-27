2a8ab4c2943e23afa43e2571e67c042d1af90d14
thelonius <thelonius@thelonius.org>
2017-12-26 21:29:38 -0500

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ var app = express();
 
 app.use(bodyParser.json());
 
+//This code posts a new todo
 app.post('/todos', (req, res) => {
   var todo = new Todo ({
     text: req.body.text
@@ -21,6 +22,17 @@ app.post('/todos', (req, res) => {
   });
 });
 
+//This code gets all todos
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  })
+});
+
 app.listen(8080, () => {
   console.log('Started on port 8080');
 });
+
+module.exports = {app};
